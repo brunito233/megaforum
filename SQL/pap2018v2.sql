adcabd445548fa4e858b8f4a18aa641b887781ae
brunito233 <49983740+brunito233@users.noreply.github.com>
2019-06-12 10:56:57 +0100

    bd

--- SQL/pap2018v2.sql
+++ SQL/pap2018v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Maio-2019 às 13:20
+-- Generation Time: 12-Jun-2019 às 11:44
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.11
 
@@ -38,21 +38,8 @@ CREATE TABLE `assunto` (
 --
 
 INSERT INTO `assunto` (`id_assunto`, `assunto`) VALUES
-(137, 'Como fazer ligação a base de dados com o visual basic?\r\nObrigado!'),
-(138, 'Como faço ligação a base de dados em PHP?\r\nObg'),
-(139, '\\dqdedwew'),
-(140, ''),
-(141, ''),
-(142, ''),
-(143, ''),
-(144, ''),
-(145, ''),
-(146, 'Ola gostaria de Saber com se faz um if em JS?\r\nObrigado'),
-(147, 'Ola gostaria de Saber com se faz um if em JS?\r\nObrigado'),
-(148, 'asdasd'),
-(149, 'asdasd'),
-(150, 'asdasd'),
-(151, 'asdasd');
+(170, '<p>Ola boa Tarde tou a come&ccedil;ar o 12&ordm; em informatica e gostava de ter ideias para a minha pap&nbsp;<br />Podem me dizer ideias??</p>'),
+(173, '<p>Ol&aacute; Boa Tarde eu tenho o seguinte codigo em PHP e nao consigo executar...<br />Da o seguinte erro:&nbsp;<strong style=\"font-family: \'Times New Roman\';\">Notice</strong><span style=\"font-family: \'Times New Roman\';\">: Undefined variable: nome in&nbsp;</span><strong style=\"font-family: \'Times New Roman\';\">C:\\xampp\\htdocs\\index233.php</strong><span style=\"font-family: \'Times New Roman\';\">&nbsp;on line&nbsp;</span><strong style=\"font-family: \'Times New Roman\';\">9</strong></p>\r\n<p>Codigo:</p>');
 
 -- --------------------------------------------------------
 
@@ -67,6 +54,14 @@ CREATE TABLE `bans` (
   `admin` varchar(200) NOT NULL,
   `data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `bans`
+--
+
+INSERT INTO `bans` (`id_ban`, `nome_utilizador`, `motivo`, `admin`, `data`) VALUES
+(2, 'alberto', 'Conteudo Improrio', 'Bruno Santos', '2019-05-20'),
+(3, 'rubs23', 'Conteudo Improrio', 'Bruno Santos', '2019-05-30');
 
 -- --------------------------------------------------------
 
@@ -89,15 +84,65 @@ INSERT INTO `categoria` (`id_categoria`, `categoria`, `type`) VALUES
 (2, 'PHP', 2),
 (3, 'CSS', 2),
 (4, 'JavaScript', 2),
-(5, 'Pascal\r\n', 3),
+(5, 'C++', 3),
 (6, 'C', 3),
-(7, 'C++', 3),
-(8, 'Java', 3),
-(9, 'Microsoft Acess', 4),
-(10, 'Mysql', 4),
-(11, 'Oracle\r\n', 4),
-(12, 'Sugestões, Críticas ou Dúvidas relativas', 1),
+(7, 'Java', 3),
+(8, 'Pascal', 3),
+(9, 'SQL', 4),
+(10, 'Microsoft Acess', 4),
+(11, 'Oracle ', 4),
+(12, 'Sugestões, Duvidas relativamnete ao Forum', 1),
 (13, 'Visual Basic', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `likes`
+--
+
+CREATE TABLE `likes` (
+  `id_like` int(11) NOT NULL,
+  `id_topico` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `likes`
+--
+
+INSERT INTO `likes` (`id_like`, `id_topico`, `id_utilizador`, `type`) VALUES
+(47, 39, 25, 0),
+(49, 39, 24, 0),
+(50, 40, 24, 0),
+(51, 41, 24, 0),
+(53, 42, 25, 0),
+(54, 43, 43, 0),
+(55, 43, 24, 0),
+(56, 42, 43, 0),
+(57, 46, 24, 0),
+(58, 46, 25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `likes_respostas`
+--
+
+CREATE TABLE `likes_respostas` (
+  `id_like_resposta` int(11) NOT NULL,
+  `id_resposta` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `likes_respostas`
+--
+
+INSERT INTO `likes_respostas` (`id_like_resposta`, `id_resposta`, `id_utilizador`) VALUES
+(5, 14, 24),
+(6, 14, 25),
+(7, 15, 24);
 
 -- --------------------------------------------------------
 
@@ -110,20 +155,16 @@ CREATE TABLE `respostas` (
   `resposta` varchar(10000) NOT NULL,
   `id_utilizador` int(11) NOT NULL,
   `id_topico` int(11) NOT NULL,
-  `aprovada` int(2) NOT NULL
+  `hora` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `respostas`
 --
 
-INSERT INTO `respostas` (`id_respostas`, `resposta`, `id_utilizador`, `id_topico`, `aprovada`) VALUES
-(31, 'Tens que baixar um ferramenta', 24, 22, 0),
-(32, '', 24, 22, 0),
-(33, '<p>Teste2</p>', 24, 22, 0),
-(34, '<p><span id=\"_mce_caret\" data-mce-bogus=\"1\" data-mce-type=\"format-caret\"><strong><em>asahfjhsjfsdkjfkjsdfgnksdnkfnksnksdgdsg</em></strong></span><br data-mce-bogus=\"1\"></p>', 24, 22, 0),
-(35, '<p style=\"text-align: center;\" data-mce-style=\"text-align: center;\"><strong>Ola tudo bem??</strong></p>', 24, 22, 0),
-(36, '<p>Boas</p>', 25, 23, 0);
+INSERT INTO `respostas` (`id_respostas`, `resposta`, `id_utilizador`, `id_topico`, `hora`) VALUES
+(14, '<p>OlÃ¡&nbsp;<strong>Matias</strong><span id=\"_mce_caret\" data-mce-bogus=\"1\" data-mce-type=\"format-caret\">, aqui tens a soluÃ§Ã£o:</span></p><p><span id=\"_mce_caret\" data-mce-bogus=\"1\" data-mce-type=\"format-caret\"><code></code></span><font face=\"monospace\">&lt;!DOCTYPE html&gt;<br data-mce-bogus=\"1\"></font></p><p><font face=\"monospace\">&lt;html lang=\"pt\" dir=\"ltr\"&gt;</font></p><p><font face=\"monospace\">&nbsp; &lt;head&gt;</font></p><p><font face=\"monospace\">&nbsp; &nbsp; &lt;meta charset=\"utf-8\"&gt;</font></p><p><font face=\"monospace\">&nbsp; &nbsp; &lt;title&gt;&lt;/title&gt;</font></p><p><font face=\"monospace\">&nbsp; &lt;/head&gt;</font></p><p><font face=\"monospace\">&nbsp; &lt;body&gt;</font></p><p><font face=\"monospace\">&nbsp; &nbsp; &lt;?php</font></p><p><font face=\"monospace\">&nbsp; &nbsp; &nbsp; $nome = \"Matias\";</font></p><p><font face=\"monospace\">&nbsp; &nbsp; &nbsp; echo $nome;</font></p><p><font face=\"monospace\">&nbsp; &nbsp; ?&gt;</font></p><p><font face=\"monospace\">&nbsp; &lt;/body&gt;</font></p><p><font face=\"monospace\">&lt;/html&gt;</font></p>', 24, 46, '2019-06-05 15:56:00'),
+(15, '<p>Obrigado&nbsp;<strong>Bruno Santos&nbsp;</strong>pela ajuda!<br>AbraÃ§o.&nbsp;</p>', 25, 46, '2019-06-05 15:58:09');
 
 -- --------------------------------------------------------
 
@@ -161,21 +202,7 @@ CREATE TABLE `titulo` (
 --
 
 INSERT INTO `titulo` (`id_titulo`, `titulo`) VALUES
-(139, ''),
-(140, ''),
-(147, 'asdas'),
-(148, 'asdas'),
-(149, 'asdas'),
-(150, 'asdas'),
-(143, 'asdasdasd'),
-(144, 'asdasdasd'),
-(145, 'Como se faz \"if\"?'),
-(146, 'Como se faz \"if\"?'),
-(138, 'dtfuyqgwefuigwhfe1'),
-(137, 'Ligação a base de dados'),
-(136, 'Ligação a base de dados pelo VB'),
-(141, 'Ola'),
-(142, 'Ola');
+(169, 'Erro em PHP');
 
 -- --------------------------------------------------------
 
@@ -197,9 +224,7 @@ CREATE TABLE `topicos` (
 --
 
 INSERT INTO `topicos` (`id_topico`, `id_categoria`, `id_titulo`, `id_assunto`, `id_utilizador`, `mostrar`) VALUES
-(22, 13, 136, 137, 25, 0),
-(23, 10, 137, 138, 24, 0),
-(31, 4, 145, 146, 24, 0);
+(46, 2, 169, 173, 25, 0);
 
 -- --------------------------------------------------------
 
@@ -230,21 +255,15 @@ CREATE TABLE `utilizadores` (
 --
 
 INSERT INTO `utilizadores` (`id_utilizador`, `nome_utilizador`, `email`, `password`, `foto`, `type`, `data_registo`, `sexo`, `especialidade`, `bio`, `foto_capa`, `link_facebook`, `link_web`, `link_instagram`, `activo`) VALUES
-(24, 'Bruno', 'santozito12@gmail.com', '123', 'Oxygen-Icons.org-Oxygen-Actions-document-edit.ico', 1, '2019-01-17', 'Masculino', '', 'Criador do Mega Forum e Administrador', 'default_capa.jpg', '', '', '', 1),
-(25, 'Matias', 'obramatias@gmail.com', '123', 'transferir.jpg', 0, '2019-01-01', 'Masculino', 'Passar Tempo', 'Obras e fazer cimento', 'default_capa.jpg', '', '', '', 1),
+(24, 'Bruno Miguel', 'santozito12@gmail.com', '123', 'Oxygen-Icons.org-Oxygen-Actions-document-edit.ico', 1, '2019-01-17', 'Masculino', 'Passar Tempo', 'Criador do Mega Forum e Administrador', 'default_capa.jpg', '', '', '', 1),
+(25, 'Matias', 'obramatias@gmail.com', '123', 'transferir.jpg', 0, '2019-01-01', 'Masculino', 'Passar Tempo', 'Hardware', 'default_capa.jpg', '', '', '', 1),
 (26, 'Marco Pinheiro', 'marcopinheiro3008@gmail.com', '123', 'default.png', 0, '2019-01-29', '', '', '', 'default_capa.jpg', '', '', '', 1),
 (27, 'Ricardo Antonio', 'ricardogames@gmail.com', '123', 'default.png', 0, '2019-02-01', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(28, 'Asdurbal', 'quinta@gmail.com', '123', 'default.png', 0, '2019-02-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
+(28, 'Rita Almeida', 'almeida89@gmail.com', '123', 'default.png', 0, '2019-02-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
 (30, 'Andre Antunes', 'andre233@gmail.com', '321', 'default.png', 0, '2019-02-08', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(31, 'ahjksdhasjd', '123@gmail.com', '123', 'default.png', 0, '2019-02-20', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(32, 'dasd', 'asd', 'sad', 'default.png', 0, '2019-02-20', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(35, 'asdas', '', '', 'default.png', 0, '2019-04-24', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(36, 'asd', 'asdasd', 'asdasd', 'default.png', 0, '2019-04-24', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(37, 'asd', '123', '123', 'default.png', 0, '2019-05-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(38, 'Mari Mello', 'marimello23@gmail.com', '321', 'default.png', 0, '2019-05-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(39, 'qwdq', 'qwdqw', 'qwdqd', 'default.png', 0, '2019-05-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(40, '', 'player23@gmail.com', '', 'default.png', 0, '2019-05-06', '', '', '', 'default_capa.jpg', '', '', '', 1),
-(41, 'player23317', 'assada', '321', 'default.png', 0, '2019-05-06', '', '', '', 'default_capa.jpg', '', '', '', 1);
+(38, 'Mari Mello', 'marimello23@gmail.com', '321', 'default.png', 0, '2019-05-06', 'Femenino', '', '', 'default_capa.jpg', '', '', '', 1),
+(42, 'alberto', 'runito@gmail.com', '123', 'default.png', 0, '2019-05-10', '', '', '', 'default_capa.jpg', '', '', '', 0),
+(43, 'rubs23', 'rubs23@gmail.com', '321', 'default.png', 0, '2019-05-30', 'Masculino', '', '', 'default_capa.jpg', '', '', '', 0);
 
 --
 -- Indexes for dumped tables
@@ -269,6 +288,18 @@ ALTER TABLE `bans`
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categoria`),
   ADD KEY `categoria` (`categoria`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id_like`);
+
+--
+-- Indexes for table `likes_respostas`
+--
+ALTER TABLE `likes_respostas`
+  ADD PRIMARY KEY (`id_like_resposta`);
 
 --
 -- Indexes for table `respostas`
@@ -315,13 +346,13 @@ ALTER TABLE `utilizadores`
 -- AUTO_INCREMENT for table `assunto`
 --
 ALTER TABLE `assunto`
-  MODIFY `id_assunto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+  MODIFY `id_assunto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=174;
 
 --
 -- AUTO_INCREMENT for table `bans`
 --
 ALTER TABLE `bans`
-  MODIFY `id_ban` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `categoria`
@@ -330,10 +361,22 @@ ALTER TABLE `categoria`
   MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `likes_respostas`
+--
+ALTER TABLE `likes_respostas`
+  MODIFY `id_like_resposta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `respostas`
 --
 ALTER TABLE `respostas`
-  MODIFY `id_respostas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_respostas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `seguir`
@@ -345,19 +388,19 @@ ALTER TABLE `seguir`
 -- AUTO_INCREMENT for table `titulo`
 --
 ALTER TABLE `titulo`
-  MODIFY `id_titulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+  MODIFY `id_titulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;
 
 --
 -- AUTO_INCREMENT for table `topicos`
 --
 ALTER TABLE `topicos`
-  MODIFY `id_topico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_topico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- Constraints for dumped tables
